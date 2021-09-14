8082f64133db960192d6737b223b759f2d3a4d65
Kyle Donnelly <kydonnelly@gmail.com>
2021-09-13 18:42:47 -0700

    OVP-77: Show private label on Profile TeamListCell
    - adjust padding between bulleted text as well

--- Decidim/UI/Team List/TeamListCell.swift
+++ Decidim/UI/Team List/TeamListCell.swift
@@ -13,8 +13,9 @@ class TeamListCell: CustomTableViewCell {
     @IBOutlet var titleLabel: UILabel!
     @IBOutlet var subtitleLabel: UILabel!
     
+    @IBOutlet var privacyLabel: UILabel!
     @IBOutlet var createdAtLabel: UILabel!
-    @IBOutlet var memberCoundLabel: UILabel!
+    @IBOutlet var memberCountLabel: UILabel!
     
     @IBOutlet var iconImageView: GiphyMediaView!
     
@@ -23,8 +24,15 @@ class TeamListCell: CustomTableViewCell {
         self.subtitleLabel.text = team.description
         self.iconImageView.setThumbnail(url: team.thumbnailUrl)
         
-        self.createdAtLabel.text = team.createdAt.asShortStringAgo()
-        self.memberCoundLabel.setPluralizableText(count: team.memberCount, singular: "member", plural: "members")
+        
+        self.createdAtLabel.text = "\(team.createdAt.asShortStringAgo()) •"
+        self.memberCountLabel.setPluralizableText(count: team.memberCount, singular: "member", plural: "members")
+        if team.isPrivate {
+            self.privacyLabel.text = "Private"
+            self.memberCountLabel.text = self.memberCountLabel.text?.appending(" •")
+        } else {
+            self.privacyLabel.text = nil
+        }
     }
     
 }
