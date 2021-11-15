bf90cfdd8a1fcba11e8e28eefa85318eab54efe6
Kyle Donnelly <kydonnelly@gmail.com>
2021-11-14 23:21:02 -0800

    OVP-63: support push notifications for issues, proposals, and comment lists

--- Decidim/UI/Linking/OpenURLQueue.swift
+++ Decidim/UI/Linking/OpenURLQueue.swift
@@ -13,6 +13,10 @@ class OpenURLQueue: NSObject {
     
     private enum Route {
         case team(id: Int)
+        case issue(id: Int)
+        case proposal(id: Int)
+        case issueComment(issueId: Int, commentId: Int?)
+        case proposalComment(proposalId: Int, commentId: Int?)
         
         init?(host: String, args: [String]) {
             switch host {
@@ -21,6 +25,28 @@ class OpenURLQueue: NSObject {
                     return nil
                 }
                 self = .team(id: teamId)
+            case "issues":
+                guard let first = args.first, let issueId = Int(first) else {
+                    return nil
+                }
+                self = .issue(id: issueId)
+            case "proposals":
+                guard let first = args.first, let proposalId = Int(first) else {
+                    return nil
+                }
+                self = .proposal(id: proposalId)
+            case "comments":
+                guard let first = args.first, let last = args.last, let id = Int(last) else {
+                    return nil
+                }
+                switch first {
+                case "issue":
+                    self = .issueComment(issueId: id, commentId: nil)
+                case "proposal":
+                    self = .proposalComment(proposalId: id, commentId: nil)
+                default:
+                    return nil
+                }
             default:
                 return nil
             }
@@ -84,13 +110,21 @@ class OpenURLQueue: NSObject {
         }
         
         switch route {
-        case .team:
-            guard let target = arguments.first, let teamId = Int(target) else {
-                return false
-            }
-            
+        case let .team(teamId):
             let teamVC = TeamDetailViewController.create(teamId: teamId)
             navigationController.pushViewController(teamVC, animated: true)
+        case let .issue(issueId):
+            let issueVC = IssueDetailViewController.create(issueId: issueId)
+            navigationController.pushViewController(issueVC, animated: true)
+        case let .proposal(proposalId):
+            let proposalVC = ProposalDetailViewController.create(proposalId: proposalId)
+            navigationController.pushViewController(proposalVC, animated: true)
+        case let .issueComment(issueId, commentId):
+            let commentVC = CommentListViewController.create(issueId: issueId, focusComment: nil)
+            navigationController.pushViewController(commentVC, animated: true)
+        case let .proposalComment(proposalId, commentId):
+            let commentVC = CommentListViewController.create(proposalId: proposalId, focusComment: nil)
+            navigationController.pushViewController(commentVC, animated: true)
         }
         
         return true
