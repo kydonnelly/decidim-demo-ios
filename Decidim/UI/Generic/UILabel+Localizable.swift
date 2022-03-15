ecfe1fd8430670908b081c5777ebc155ec5da7e4
Kyle Donnelly <kydonnelly@gmail.com>
2022-03-15 02:07:17 -0700

    rewrite icon appending/prepending as protocol for proper UIButton support

--- Decidim/UI/Generic/UILabel+Localizable.swift
+++ Decidim/UI/Generic/UILabel+Localizable.swift
@@ -20,16 +20,23 @@ extension UILabel {
     
 }
 
-extension UILabel {
+protocol IconTextElement {
+    var currentFont: UIFont? { get }
+    var currentText: String? { get }
+    var currentTextColor: UIColor? { get }
+    func setAttributedString(_ text: NSAttributedString)
+}
+
+extension IconTextElement {
     
     func appendIcon(_ icon: VotionIcon) {
-        let regularText = self.text?.appending(" ") ?? ""
+        let regularText = self.currentText?.appending(" ") ?? ""
         self.setText(regularText, with: icon, at: regularText.count)
     }
     
     func prependIcon(_ icon: VotionIcon) {
         let regularText: String
-        if let text = self.text {
+        if let text = self.currentText {
             regularText = " \(text)"
         } else {
             regularText = ""
@@ -41,7 +48,7 @@ extension UILabel {
     @discardableResult
     private func setText(_ text: String, with icon: VotionIcon, at index: Int) -> Bool {
         guard let iconCode = VotionIcon.iconCode(for: icon) else { return false }
-        guard let font = self.font, let color = self.textColor else { return false }
+        guard let font = self.currentFont, let color = self.currentTextColor else { return false }
         
         let regularAttributes: [NSAttributedString.Key: Any] = [.font: font, .foregroundColor: color]
         
@@ -52,20 +59,48 @@ extension UILabel {
         let attributedText = NSMutableAttributedString(string: text, attributes: regularAttributes)
         attributedText.insert(iconText, at: index)
         
-        self.attributedText = attributedText
+        self.setAttributedString(attributedText)
         return true
     }
     
 }
 
-extension UIButton {
+extension UILabel: IconTextElement {
     
-    func appendIcon(_ icon: VotionIcon) {
-        self.titleLabel?.prependIcon(icon)
+    var currentFont: UIFont? {
+        return self.font
     }
     
-    func prependIcon(_ icon: VotionIcon) {
-        self.titleLabel?.prependIcon(icon)
+    var currentText: String? {
+        return self.text
+    }
+    
+    var currentTextColor: UIColor? {
+        return self.textColor
+    }
+    
+    func setAttributedString(_ text: NSAttributedString) {
+        self.attributedText = text
+    }
+    
+}
+
+extension UIButton: IconTextElement {
+    
+    var currentFont: UIFont? {
+        return self.titleLabel?.font
+    }
+    
+    var currentText: String? {
+        return self.titleLabel?.text
+    }
+    
+    var currentTextColor: UIColor? {
+        return self.titleLabel?.textColor
+    }
+    
+    func setAttributedString(_ text: NSAttributedString) {
+        self.setAttributedTitle(text, for: .normal)
     }
     
 }
--- Decidim/UI/Team Detail/TeamMemberListCell.swift
+++ Decidim/UI/Team Detail/TeamMemberListCell.swift
@@ -20,14 +20,19 @@ class TeamMemberListCell: CustomTableViewCell {
     
     private var inviteBlock: InviteBlock?
     
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+        self.inviteButton.setTitle("Invite", for: .normal)
+        self.inviteButton.prependIcon(.add_user)
+    }
+    
     func setup(detail: TeamDetail, canInvite: Bool, inviteBlock: InviteBlock?, tappedProfileBlock: ProfileBlock?) {
         let activeMembers = detail.memberList.filter { $0.status == .active }.map { $0.user_id }
         self.numMembersLabel.text = "\(activeMembers.count) Members"
         
         self.inviteBlock = inviteBlock
         self.inviteButton.isHidden = !canInvite
-        self.inviteButton.setTitle("Invite", for: .normal)
-        self.inviteButton.prependIcon(.add_user)
 
         ProfileInfoDataController.shared().refresh { [weak self] dc in
             guard let self = self else { return }
